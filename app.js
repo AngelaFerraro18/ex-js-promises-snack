0bfd2ee6054107153c4c82dbf40ad698b85950f8
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-06-27 12:08:18 +0200

    feat: snack 2 completed

--- app.js
+++ app.js
@@ -41,3 +41,68 @@ getPostUser(1)
         console.log("Autore:", post.user.firstName, post.user.lastName);
     })
     .catch(error => console.error(error))
+
+
+
+// Crea la funzione lanciaDado() che restituisce una Promise che, dopo 3 secondi, genera un numero casuale tra 1 e 6. Tuttavia, nel 20% dei casi, il dado si "incastra" e la Promise va in reject.
+// 🎯 Bonus: HOF con closure per memorizzare l'ultimo lancio
+// Modifica la funzione in creaLanciaDado(), che restituisce una closure che memorizza l'ultimo risultato. Se il numero esce due volte di fila, stampa "Incredibile!".
+
+
+function lanciaDado() {
+    return new Promise((resolve, reject) => {
+        console.log('Sto lanciando il dado...')
+        setTimeout(() => {
+            const probabilita = Math.random();
+
+            if (probabilita < 0.2) {
+                reject('Il dado si è incastrato!')
+            } else {
+                const numero = Math.floor(Math.random() * 6) + 1;
+                resolve(`Il numero è: ${numero}`)
+            }
+        }, 3000)
+    })
+}
+
+lanciaDado()
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
+
+
+function creaLanciaDado() {
+
+    let ultimoLancio = null;
+
+    return function () {
+        return new Promise((resolve, reject) => {
+            console.log('Sto lanciando il dado...')
+            setTimeout(() => {
+                const probabilita = Math.random();
+
+                if (probabilita < 0.2) {
+                    reject('Il dado si è incastrato!')
+                    return;
+                }
+
+                const numero = Math.floor(Math.random() * 6) + 1;
+
+                if (numero === ultimoLancio) {
+                    console.log('Incredibile!')
+                }
+
+                ultimoLancio = numero;
+                resolve(numero)
+            }, 3000)
+        })
+    }
+
+}
+
+const lancio = creaLanciaDado();
+
+lancio()
+    .then(num => console.log("Primo lancio:", num))
+    .then(() => lancio())
+    .then(num => console.log("Secondo lancio:", num))
+    .catch(err => console.error(err))
